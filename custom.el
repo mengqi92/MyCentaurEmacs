8d7a70df4400a4a888a35d2d6bcb64c87896e2ac
Mengqi Pei <mengqipei@gmail.com>
2022-08-29 00:31:06 +0800

    Treak Chinese font display

--- custom.el
+++ custom.el
@@ -35,41 +35,41 @@
   "Setup fonts."
   (when (display-graphic-p)
     ;; Set default font
-    (cl-loop for font in '("Roboto" "Fira Code" "Bookerly" "Cascadia Code" "Jetbrains Mono"
-                           "SF Mono" "Hack" "Source Code Pro" "Menlo"
-                           "Monaco" "DejaVu Sans Mono" "Consolas")
-             when (font-installed-p font)
-             return (set-face-attribute 'default nil
-                                        :family font
-                                        :height (cond (sys/macp 180)
-                                                      (sys/win32p 110)
-                                                      (t 160))))
+    ;; (cl-loop for font in '("Roboto" "Fira Code" "Bookerly" "Cascadia Code" "Jetbrains Mono"
+    ;;                        "SF Mono" "Hack" "Source Code Pro" "Menlo"
+    ;;                        "Monaco" "DejaVu Sans Mono" "Consolas")
+    ;;          when (font-installedfon-t p)
+    ;;          return (set-face-attribute 'nil default
+    ;;                                     :family font
+    ;;                                     :height (cond (sys/macp 180)
+    ;;                                                   (sys/win32p 110)
+    ;;                                                   (t 160))))
 
-    ;; Set mode-line font
-    ;; (cl-loop for font in '("Menlo" "SF Pro Display" "Helvetica")
+    ;; ;; Set mode-line font
+    ;; ;; (cl-loop for font in '("Menlo" "SF Pro Display" "Helvetica")
+    ;; ;;          when (font-installed-p font)
+    ;; ;;          return (progn
+    ;; ;;                   (set-face-attribute 'mode-line nil :family font :height 120)
+    ;; ;;                   (when (facep 'mode-line-active)
+    ;; ;;                     (set-face-attribute 'mode-line-active nil :family font :height 120))
+    ;; ;;                   (set-face-attribute 'mode-line-inactive nil :family font :height 120)))
+
+    ;; ;; Specify font for all unicode characters
+    ;; (cl-loop for font in '("Noto Sans" "Segoe UI Symbol" "Symbola" "Symbol")
     ;;          when (font-installed-p font)
-    ;;          return (progn
-    ;;                   (set-face-attribute 'mode-line nil :family font :height 120)
-    ;;                   (when (facep 'mode-line-active)
-    ;;                     (set-face-attribute 'mode-line-active nil :family font :height 120))
-    ;;                   (set-face-attribute 'mode-line-inactive nil :family font :height 120)))
+    ;;          return (set-fontset-font t 'unicode font nil 'prepend))
 
-    ;; Specify font for all unicode characters
-    (cl-loop for font in '("Noto Sans" "Segoe UI Symbol" "Symbola" "Symbol")
-             when (font-installed-p font)
-             return (set-fontset-font t 'unicode font nil 'prepend))
+    ;; ;; Emoji
+    ;; (cl-loop for font in '("Noto Color Emoji" "Apple Color Emoji")
+    ;;          when (font-installed-p font)
+    ;;          return (if (>= emacs-major-version 28)
+    ;;                     (set-fontset-font t 'emoji (font-spec :family font) nil 'prepend)
+    ;;                   (set-fontset-font t 'symbol (font-spec :family font) nil 'prepend)))
 
-    ;; Emoji
-    (cl-loop for font in '("Noto Color Emoji" "Apple Color Emoji")
-             when (font-installed-p font)
-             return (if (>= emacs-major-version 28)
-                        (set-fontset-font t 'emoji (font-spec :family font) nil 'prepend)
-                      (set-fontset-font t 'symbol (font-spec :family font) nil 'prepend)))
-
-    ;; Specify font for Chinese characters
-    (cl-loop for font in '("Noto Sans CJK SC" "LXGW WenKai" "WenQuanYi Micro Hei" "Microsoft Yahei")
-             when (font-installed-p font)
-             return (set-fontset-font t '(#x4e00 . #x9fff) font))
+    ;; ;; Specify font for Chinese characters
+    ;; (cl-loop for font in '("Noto Sans CJK SC" "LXGW WenKai" "WenQuanYi Micro Hei" "Microsoft Yahei")
+    ;;          when (font-installed-p font)
+    ;;          return (set-fontset-font t '(#x4e00 . #x9fff) font))
     (cnfonts-mode 1)))
 
 (centaur-setup-fonts)
--- lisp/init-chinese.el
+++ lisp/init-chinese.el
@@ -6,6 +6,7 @@
 ;;
 
 ;;; Code:
+;; (cnfonts-mode 1)
 
 (use-package cnfonts
   :config
@@ -18,6 +19,23 @@
   (setq cnfonts-profiles
         '("Reading" "Org Mode" "Programming"))
   )
+
+(defvar my-line-spacing-alist
+  '((9 . 0.1) (10 . 0.9) (11.5 . 0.2)
+    (12.5 . 0.2) (14 . 0.2) (16 . 0.2)
+    (18 . 0.2) (20 . 1.0) (22 . 0.2)
+    (24 . 0.2) (26 . 0.2) (28 . 0.2)
+    (30 . 0.2) (32 . 0.2)))
+
+(defun my-line-spacing-setup (fontsizes-list)
+  (let ((fontsize (car fontsizes-list))
+        (line-spacing-alist (cl-copy-list my-line-spacing-alist)))
+    (dolist (list line-spacing-alist)
+      (when (= fontsize (car list))
+        (setq line-spacing-alist nil)
+        (setq-default line-spacing (cdr list))))))
+
+(add-hook 'cnfonts-set-font-finish-hook #'my-line-spacing-setup)
 
 (provide 'init-chinese)
 
